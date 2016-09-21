874ac245fa123a0b3749f9534fa4a231a7451bae
seolin <kin3342@naver.com>
2016-09-21 16:20:44 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyparser = require("body-parser");
+var methodoverride = require("method-override");
 var app = express();
 
 mongoose.connect(process.env.MONGO_DB);
@@ -18,6 +19,7 @@ app.set("view engine","ejs");
 app.use(express.static(__dirname + '/public'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(methodoverride("_method"));
 
 var contactschema = mongoose.Schema({
   name:{type:String, required:true, unique:true},
@@ -45,8 +47,36 @@ app.get("/contacts/new",function(req,res){
   res.render("contacts/new");
 });
 
+app.get("/contacts/:id",function(req,res){
+  contact.findOne({_id:req.params.id},function(err,contact){
+    if(err) return res.json(err);
+    res.render("contacts/show",{contact:contact});
+  });
+});
+
+app.get("/contacts/:id/edit",function(req,res){
+  contact.findOne({_id:req.params.id},function(err,contact){
+    if(err) return res.json(err);
+    res.render("contacts/edit",{contact:contact});
+  });
+});
+
 app.post("/contacts",function(req,res){
   contact.create(req.body,function(err,contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts");
+  });
+});
+
+app.put("/contacts/:id",function(req,res){
+  contact.findOneAndUpdate({_id:req.params.id},req.body,function(err,contact){
+    if(err) return res.json(err);
+    res.redirect("/contacts/"+req.params.id);
+  });
+});
+
+app.delete("/contacts/:id",function(req,res){
+  contact.remove({_id:req.params.id},function(err,contact){
     if(err) return res.json(err);
     res.redirect("/contacts");
   });
